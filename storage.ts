6fbb4f66731b18f9545e64455ef11ae75e7af51b
Alexey Eremin <a.eremin.msu@gmail.com>
2019-03-02 19:51:40 +0100

    Only say "votes" where it's really meant

--- storage.ts
+++ storage.ts
@@ -3,7 +3,7 @@ import { DatastoreRequest } from '@google-cloud/datastore/request';
 import { MessageVotes, UserStats } from './util';
 
 export interface DatabaseInterface<T> {
-  saveDatastoreEntry(dbKey: string, votes: T): void;
+  saveDatastoreEntry(dbKey: string, entity: T): void;
   readDatastoreEntry(dbKey: string): Promise<T | undefined>;
   updateDatastoreEntry(dbKey: string, modifier: (v: T | undefined) => boolean): Promise<T | undefined>
 }
@@ -12,8 +12,8 @@ class DatastoreConnector<T> implements DatabaseInterface<T> {
   private datastore = new Datastore();
   constructor(private readonly kDatastoreKind, private maxRetries: number = 10) { }
 
-  public saveDatastoreEntry(dbKey: string, votes: T) {
-    return this.saveDatastoreEntryImpl(this.datastore, dbKey, votes);
+  public saveDatastoreEntry(dbKey: string, entity: T) {
+    return this.saveDatastoreEntryImpl(this.datastore, dbKey, entity);
   }
 
   public readDatastoreEntry(dbKey: string): Promise<T | undefined> {
@@ -25,16 +25,16 @@ class DatastoreConnector<T> implements DatabaseInterface<T> {
       try {
         const transaction = this.datastore.transaction();
         await transaction.run();
-        const votes = await this.readDatastoreEntryImpl(transaction, dbKey);
-        if (!modifier(votes) || !votes) {
+        const entity = await this.readDatastoreEntryImpl(transaction, dbKey);
+        if (!modifier(entity) || !entity) {
           await transaction.rollback();
           return undefined;
         }
-        await this.saveDatastoreEntryImpl(transaction, dbKey, votes);
+        await this.saveDatastoreEntryImpl(transaction, dbKey, entity);
         const commitResult = await transaction.commit();
         if (commitResult.length && commitResult[0].mutationResults.length &&
           !commitResult[0].mutationResults[0].conflictDetected)
-          return votes;
+          return entity;
         console.warn('Retrying because of conflict');
       } catch (e) {
         console.error(`Caught error: ${e}, let's retry`);
@@ -43,10 +43,10 @@ class DatastoreConnector<T> implements DatabaseInterface<T> {
     return undefined;
   }
 
-  private async saveDatastoreEntryImpl(dsInterface: DatastoreRequest | undefined, dbKey: string, votes: T) {
+  private async saveDatastoreEntryImpl(dsInterface: DatastoreRequest | undefined, dbKey: string, entity: T) {
     const task = {
       key: this.datastore.key([this.kDatastoreKind, dbKey]),
-      data: votes
+      data: entity
     };
     if (!dsInterface)
       dsInterface = this.datastore;
