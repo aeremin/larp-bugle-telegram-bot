9fad7d63d562eceed64b8f86c65a906fbab570a5
Alexey Eremin <a.eremin.msu@gmail.com>
2021-08-29 01:25:03 +0200

    Enable webhook support instead of polling

--- main.ts
+++ main.ts
@@ -10,8 +10,16 @@ dotenv.config();
 // See https://github.com/yagop/node-telegram-bot-api/issues/319
 process.env.NTBA_FIX_319 = "X";
 
-const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN!, { polling: true });
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN!);
 setUpBotBehavior(bot, new MessageVotesDatabase(), new UserStatsDatabase(), new NewsArticlesDatabase(), new ReporterStateDatabase(), messages.getConfig());
 
 
+bot.setWebHook(process.env.WEBHOOK_URL!).then(() => console.log('Webhook set'));
 
+export const botFunction = async (req: { body: TelegramBot.Update }, res: any) => {
+  try {
+    await bot.processUpdate(req.body);
+  } finally {
+    res.status(200).end();
+  }
+};
